fe3c6c432282fdf85e835e03cd23b89eda5a5d66
RevREB <rbaker@rbaker-laptop.tagged.com>
2014-02-07 01:55:39 -0800

    updated auto pull script

--- scripts/gitpull.sh
+++ scripts/gitpull.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 cd /cloudinabox
-git pull master
+output1=$(git pull master)
+output2=$(echo $output2 | tr -d ‘ ‘)
+if [ $output2 = “Alreadyup-to-date.” ];
+then
+  echo “OMG it’s up2date!”;
+else
+  echo “Reloading Puppetmaster configs.”
+  service puppetmaster restart
+fi
